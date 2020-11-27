0e3c66f23a5dea00479d21a76b9b051cc7ce26ce
Andres G. Carmona <ing.andres.carmona@gmail.com>
2020-11-26 21:22:09 -0600

    Installed lravel-mix-purgecss library. Added webpack configuration file. Updated laravel mix config file.

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,15 +1,20 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-purgecss')
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+	.extract()
+
+mix.sass('resources/sass/app.scss', 'public/css')
+	.options({
+		processCssUrls: false,
+		postCss: [],
+	})
+
+mix.webpackConfig(require('./webpack.config'))
+
+if(mix.inProduction()) {
+	mix.purgeCss().sourceMaps()
+}
+
+mix.version()
